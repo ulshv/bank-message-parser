e77bd4f32cb56f36ff4c169d20d429400f4546b5
Sergey Ulyashev <ulyashev@gmail.com>
2017-05-29 23:53:00 +0300

    Add Patterns and TestCases for Bank #002

--- src/banks/russia/002_tinkoff/parser.ts
+++ src/banks/russia/002_tinkoff/parser.ts
@@ -1,9 +1,41 @@
 // Tinkoff parser
 
-import { Message, Pattern, Transaction, Parser } from '../../../types';
+import { Message, Pattern, Transaction, Parser, ParsersById } from '../../../types';
+
+const parser200 = (message: Message, pattern: Pattern): Transaction | void => {
+  const data = message.match(pattern.regexp);
+  if (!(data && data.length === 7)) return;
+
+  const action = data[1];
+  const balance = parseFloat(data[6]);
+  const card = data[2];
+  const datetime = data[5];
+  const type = 'outcome';
+  const value = parseFloat(data[3]);
+  const vendor = data[4];
+
+  const transaction: Transaction = {
+    action,
+    balance,
+    card,
+    datetime,
+    type,
+    value,
+    vendor,
+  }
+
+  return transaction;
+}
 
 const parseMessage = (message: Message, pattern: Pattern): Transaction | void => {
-  return;
+  const parser = parsersById[pattern.id];
+  if (!parser) return;
+
+  return parser(message, pattern);
+}
+
+const parsersById: ParsersById = {
+  200: parser200
 }
 
 const parser: Parser = {
--- src/banks/russia/002_tinkoff/patterns.ts
+++ src/banks/russia/002_tinkoff/patterns.ts
@@ -3,11 +3,11 @@
 import { Pattern } from '../../../types';
 
 const patterns: Pattern[] = [
-  // {
-  //   id: 200,
-  //   bank_id: 2,
-  //   regexp: /(Pokupka).*Dostupno.*Tinkoff\.ru/
-  // },
+  {
+    id: 200,
+    bank_id: 2,
+    regexp: /(?:(Pokupka))\. Karta (\*\d+)\. Summa (\d+(?:.\d+)?) RUB\. ([\w\d\s.,а-яё]*?)\. (\d+\.\d+\.\d+ \d+:\d+)\. Dostupno (\d+(?:\.\d+)?) RUB\. Tinkoff\.ru/i
+  },
 ];
 
 export default patterns;
