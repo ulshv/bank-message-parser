253348157887640d22c6522418a3c76e2eeffa1f
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-03 01:42:09 +0300

    Add support of online-bank check for 'ru.sberbank' Bank

--- src/banks/ru/sberbank/patterns.ts
+++ src/banks/ru/sberbank/patterns.ts
@@ -17,6 +17,20 @@ const patterns: Pattern[] = [
       vendor   : data[6] || null,
     } as Transaction),
   },
+  {
+    id: 2,
+    bank_id: "ru.sberbank",
+    regexp: /(\d{2}[а-я]+)(\d{2})? \d{2}[а-я]+(\d{2}) \d+ (?:(плата.*|sberbank onl@in.*)|([\wа-яё\s]+)) (RUR) (\d+(?:\.\d+)?) (\d+(?:\.\d+)?)(CR)?/i,
+    parser: (data: RegExpMatchArray): Transaction => ({
+      action   : data[4] || null,
+      balance  : null,
+      card     : null,
+      datetime : data[1] + (data[2] || data[3]),
+      type     : data[9] ? 'income' : 'outcome',
+      value    : parseFloat(data[8]),
+      vendor   : data[5] || null,
+    } as Transaction),
+  },
 ];
 
 export default patterns;
