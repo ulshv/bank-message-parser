582d1c56857666224faf9a4dab9d22c978503b63
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-03 03:40:58 +0300

    Uncomment 'testCases' and 'unsupported' params for 'ru.sberbank' module

--- src/banks/ru/sberbank/index.ts
+++ src/banks/ru/sberbank/index.ts
@@ -1,14 +1,15 @@
 // Sberbank
+
 import { Bank, TestCase } from '../../../types';
 import patterns from './patterns';
-// const testCases = process.env.NODE_ENV === 'production' ? null : require('./test-cases.json');
-// const unsupported = process.env.NODE_ENV === 'production' ? null : require('./unsupported.json');
+const testCases = process.env.NODE_ENV === 'production' ? null : require('./test-cases.json');
+const unsupported = process.env.NODE_ENV === 'production' ? null : require('./unsupported.json');
 
 const bank: Bank = {
   id: 'ru.sberbank',
   patterns,
-  testCases: null,
-  unsupported: null,
+  testCases,
+  unsupported,
 };
 
 export default bank;
