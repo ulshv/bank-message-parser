de0d439117084b18273e80026ecb3479298c33a8
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-03 03:38:26 +0300

    Update 'description' for 'ru.raiffeisen', pattern: 1

--- src/banks/ru/raiffeisen/patterns.ts
+++ src/banks/ru/raiffeisen/patterns.ts
@@ -6,12 +6,12 @@ const patterns: Pattern[] = [
   {
     id: 1,
     bank_id: "ru.raiffeisen",
-    regexp: /Karta (\*\d+); (?:(Pokupka)): ([\w\d\s.,\/а-яё]*?); (\d+(?:\.\d+)?) RUR; Data: (\d+\/\d+\/\d+); Dostupny Ostatok: (\d+(?:\.\d+)?) RUR. Raiffeisenbank/i,
+    regexp: /Karta (\*\d+); (Pokupka): ([\w\d\s.,\/а-яё]*?); (\d+(?:\.\d+)?) RUR; Data: (\d+\/\d+\/\d+); Dostupny Ostatok: (\d+(?:\.\d+)?) RUR. Raiffeisenbank/i,
     parser: (data: RegExpMatchArray): Transaction => ({
       balance     : parseFloat(data[6]),
       card        : data[1],
       datetime    : data[5],
-      description : data[2],
+      description : data[3] ? `Покупка в ${data[3]}` : null,
       flow        : '-',
       value       : parseFloat(data[4]),
       vendor      : data[3] || null,
