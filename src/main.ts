d77692067278e4fa03a4f0e317c16f428eb6a9a7
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-06 05:25:10 +0300

    Update types and main functions to work with unix time and timezone

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,25 @@
 import banks, { banksById, patterns } from './banks';
-import { Message, Transaction, Pattern } from './types';
+import { Message, Transaction, Props, Pattern } from './types';
 
-export const getPatternFromMessage = (message: Message, bankId? : string)
-: Pattern | void =>
-  patterns.find(pattern => (
+export const getPatternFromMessage = (message: Message, props: Props = {})
+: Pattern | void => {
+  const { bankId } = props;
+  return patterns.find(pattern => (
     (bankId ? pattern.bank_id === bankId : true) &&
     pattern.regexp.test(message)
   ));
-
-export const getTransaction = (message: Message, pattern: Pattern)
-: Transaction | void => {
-  const data = message.match(pattern.regexp);
-  if (data) return pattern.parser(data);
 }
 
-const main = (message: Message, bankId? : string)
+export const getTransaction = (message: Message, pattern: Pattern, props: Props = {})
 : Transaction | void => {
-  const pattern = getPatternFromMessage(message);
-  if (pattern) return getTransaction(message, pattern);
+  const data = message.match(pattern.regexp);
+  if (data) return pattern.parser(data, props);
+}
+
+const main = (message: Message, props?: Props)
+: Transaction | void => {
+  const pattern = getPatternFromMessage(message, props);
+  if (pattern) return getTransaction(message, pattern, props);
 }
 
 export default main;
