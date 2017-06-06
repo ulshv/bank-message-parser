6d014d2216c6b97570b246f54b62ff5c0ef90c61
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-06 05:23:54 +0300

    Add unix time support for 'ru.raiffeisen' Patterns and TestCases

--- src/banks/ru/raiffeisen/patterns.ts
+++ src/banks/ru/raiffeisen/patterns.ts
@@ -1,22 +1,29 @@
 // Copyright 2017-present Sergey Ulyashev and Contributors. Licensed under the Apache License v2.0
 // You may not use this file except in compliance with the License (http://www.apache.org/licenses/LICENSE-2.0)
 
-import { Pattern, Transaction } from '../../../types';
+import { Pattern, Transaction, Props } from '../../../types';
+import { getMomentDate } from '../../../utils';
 
 const patterns: Pattern[] = [
   {
     id: 1,
     bank_id: "ru.raiffeisen",
     regexp: /Karta (\*\d+); (Pokupka): ([\w\d\s.,\/а-яё]*?); (\d+(?:\.\d+)?) RUR; Data: (\d+\/\d+\/\d+); Dostupny Ostatok: (\d+(?:\.\d+)?) RUR. Raiffeisenbank/i,
-    parser: (data: RegExpMatchArray): Transaction => ({
-      balance     : parseFloat(data[6]),
-      card        : data[1],
-      datetime    : data[5],
-      description : 'Покупка',
-      flow        : '-',
-      value       : parseFloat(data[4]),
-      vendor      : data[3] || null,
-    } as Transaction),
+
+    parser: (data: RegExpMatchArray, props: Props): Transaction => {
+      const date = getMomentDate(data[5], 'DD/MM/YYYY', props.timezone);
+
+      return ({
+        balance     : parseFloat(data[6]),
+        card        : data[1],
+        datetime    : date.format(),
+        description : 'Покупка',
+        flow        : '-',
+        unixtime    : date.unix(),
+        value       : parseFloat(data[4]),
+        vendor      : data[3] || null,
+      } as Transaction)
+    },
   },
 ];
 
