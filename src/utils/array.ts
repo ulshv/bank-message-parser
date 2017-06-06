555ff81cce800c8fdb3ff7ee7f2393b7a2802f62
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-06 05:20:43 +0300

    Split utils.ts to multiple files in ./utils directory

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -9,17 +9,3 @@ export function arrayToObjectWithStringIds <T extends { id: string }> (arr: T[])
   arr.forEach(item => objectWithIds[item.id] = item);
   return objectWithIds;
 }
-
-/** Trim too long messages using '...' separator in the middle */
-export const trimText = (
-  text: string,
-  limit: number = 55,
-  separator: string = ' ... '
-): string => {
-  if (text.length <= limit) return text;
-  const half = Math.round((limit - separator.length) / 2)
-  return text.substr(0, half) + separator + text.substr(text.length - half, text.length);
-}
-
-/** Returns string with capitalized first character */
-export const capitalize = (str?: string) => str && str.charAt(0).toUpperCase() + str.slice(1)
