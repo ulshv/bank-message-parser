fca4aa2ff118b3d0d4cb0ede561742263c0700d9
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-11 00:18:16 +0300

    Add tests and create/improve functions in utils/date.ts; Add comments

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,6 @@
 import * as moment from 'moment';
 
+/** Key - month name in English, value - month name in other languages (MMM format) */
 const engMonthsRegexpMap: { [month: string]: RegExp } = {
   jan: /янв/i,
   feb: /фев/i,
@@ -15,10 +16,12 @@ const engMonthsRegexpMap: { [month: string]: RegExp } = {
   dec: /дек/i,
 };
 
+/** Provide month name in other language and get month name in English (MMM format) */
 export const getEngMonth = (month: string): string | void => (
   Object.keys(engMonthsRegexpMap).find(k => engMonthsRegexpMap[k].test(month))
 );
 
+/** Create Moment.js object with better timezone handling */
 export const getMomentDate = (
   date: string,
   format: string,
@@ -27,8 +30,50 @@ export const getMomentDate = (
   moment(date + timezone, format + "Z").utcOffset(timezone)
 )
 
+/**
+ * Check if Moment object which was created only using day and month
+ * should have previous year's value (e.g. 2016, not 2017).
+ * By default it has year from current system time.
+ */
 export const isLastYear = (date: moment.Moment): boolean => {
   const now = moment().utcOffset(date._tzm || 0)
-  const dateWithTZ = date.utcOffset(date._tzm || 0);
+  const dateWithTZ = moment(date).utcOffset(date._tzm || 0);
   return now.month() < dateWithTZ.month()
+}
+
+/**
+ * Get timezone value in '+-HH:mm' format or 'Z' (UTC).
+ * Provide number between -16 and 16 with timezone hours
+ * or timezone string in '+-HH:mm' format.
+ *
+ * Examples:
+ * 5.5 will return '+05:30'
+ * '+07:30' will return '+07:30'
+ * Any other input value will return 'Z' (UTC +00:00)
+ */
+export const parseTimezone = (input?: number | string | null): string => {
+  if (typeof input === 'number' && (input > -16) && (input < 16))
+    return numberToTimezone(input);
+
+  if (typeof input === 'string' && /^[+-]\d{2}:\d{2}$/.test(input))
+    return input;
+
+  return 'Z' // 'Z' is UTC (+00:00)
+}
+
+/** Return string with 2 digits. Add '0' before number if it's less than 10 */
+const add0 = (num: number): string => (num < 10 ? '0' : '') + num;
+
+/**
+ * Convert number to timezone in +-HH:mm format (or 'Z' for 0).
+ * Examples: 5 => '+05:00', -10.5 => '-10:30', 0 => 'Z'
+ */
+export const numberToTimezone = (num: number): string => {
+  if (!num) return 'Z';
+
+  const hours = Math.floor(Math.abs(num));
+  const mins = parseInt(((Math.abs(num) % 1) * 60).toFixed()) ;
+  const sign = num < 0 ? '-' : '+';
+
+  return `${sign}${add0(hours)}:${add0(mins)}`
 }
--- tests/utils/date.spec.ts
+++ tests/utils/date.spec.ts
@@ -1,6 +1,11 @@
 import { expect } from "chai";
 import * as moment from 'moment';
-import { isLastYear, getMomentDate } from '../../src/utils/date';
+import {
+  isLastYear,
+  getMomentDate,
+  numberToTimezone,
+  parseTimezone,
+} from '../../src/utils/date';
 
 describe("utils/date.ts", () => {
   describe("isLastYear()", () => {
@@ -12,7 +17,6 @@ describe("utils/date.ts", () => {
       { date: '01 JAN 01:00+10:00', result: false },
       { date: '01 JAN 01:00-10:00', result: false },
     ];
-
     testCases.forEach(tc => {
       it(`isLastYear('${tc.date}') should return ${tc.result}`, () => {
         const date = moment(tc.date, 'DD MMM HH:mmZ');
@@ -21,6 +25,7 @@ describe("utils/date.ts", () => {
     });
   });
 
+
   describe('getMomentDate()', () => {
     const format = 'YYYY-MM-DD HH:mm';
     const testCases = [
@@ -28,7 +33,6 @@ describe("utils/date.ts", () => {
       { date: '2017-05-20 12:00', timezone: '+10:00', result: '2017-05-20T12:00:00+10:00' },
       { date: '2017-05-20 12:00', timezone: '-10:00', result: '2017-05-20T12:00:00-10:00' },
     ];
-
     testCases.forEach(tc => {
       it(`getMomentDate('${tc.date}', '${tc.timezone}') should return ${tc.result}`, () => {
         const date = getMomentDate(tc.date, format, tc.timezone);
@@ -36,4 +40,49 @@ describe("utils/date.ts", () => {
       });
     })
   });
+
+
+  describe('numberToTimezone()', () => {
+    const testCases = [
+      { number: 0,     timezone: 'Z' },
+      { number: 5,     timezone: '+05:00' },
+      { number: 5.5,   timezone: '+05:30'},
+      { number: 5.123, timezone: '+05:07'},
+      { number: -5,    timezone: '-05:00' },
+      { number: -5.5,  timezone: '-05:30'},
+    ];
+    testCases.forEach(tc => {
+      it(`numberToTimezone(${tc.number}) should return ${tc.timezone}`, () => {
+        const timezone = numberToTimezone(tc.number);
+        expect(timezone).to.be.equal(tc.timezone);
+      });
+    });
+  });
+
+
+  describe('parseTimezone', () => {
+    const testCases = [
+      { input: 0,        timezone: 'Z' },
+      { input: 100,      timezone: 'Z' },
+      { input: null,     timezone: 'Z' },
+      { input: 1,        timezone: '+01:00' },
+      { input: 7.5,      timezone: '+07:30' },
+      { input: 10,       timezone: '+10:00' },
+      { input: -1,       timezone: '-01:00' },
+      { input: -7.5,     timezone: '-07:30' },
+      { input: -10.5,    timezone: '-10:30' },
+      { input: '+00:00', timezone: '+00:00' },
+      { input: '+05:00', timezone: '+05:00' },
+      { input: '-10:30', timezone: '-10:30' },
+      { input: '05:00',  timezone: 'Z' },
+      { input: '05',     timezone: 'Z' },
+    ];
+    testCases.forEach(tc => {
+      it(`getTimezoneFromNumber(${tc.input}) should return ${tc.timezone}`, () => {
+        const timezone = parseTimezone(tc.input);
+        expect(timezone).to.be.equal(tc.timezone);
+      });
+    });
+  });
+
 });
