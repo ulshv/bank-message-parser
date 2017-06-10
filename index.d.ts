e251560081d6e31bd3845730826543bd040f5360
Sergey Ulyashev <ulyashev@gmail.com>
2017-06-11 01:51:02 +0300

    Update declarations in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -8,8 +8,8 @@ import {
 export const getPatternFromMessage:
   (message: string, bankId?: string) => Pattern | void;
 export const getTransaction:
-  (message: string, pattern: Pattern) => Transaction | void;
+  (message: string, pattern: Pattern, timezone: string) => Transaction | void;
 declare const main:
-  (message: string, bankId?: string) => Transaction | void;
+  (message: string, props?: { bankId?: string, timezone?: number | string | null }) => Transaction | void;
 
 export default main;
